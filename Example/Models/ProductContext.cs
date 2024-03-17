f0d9dae3c7d1fd43a59c782bc0125a7d6904461e
Magomedali Gajiev <mag198421@gmail.com>
2024-03-17 22:47:46 +0300

    Исправил изменения

--- Example/Models/ProductContext.cs
+++ Example/Models/ProductContext.cs
@@ -19,40 +19,36 @@ namespace Example.Models
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<Product>(entity =>
             {
                 entity.ToTable("Products");
-
+                entity.Property(x => x.Id).ValueGeneratedOnAdd(); // Указание, что Id будет генерироваться автоматически
                 entity.HasKey(x => x.Id);
                 entity.HasIndex(x => x.Name).IsUnique();
-
                 entity.Property(e => e.Name)
                       .HasColumnName("ProductName")
                       .HasMaxLength(255)
                       .IsRequired();
-
                 entity.Property(e => e.Description)
                     .HasColumnName("Deccription")
                     .HasMaxLength(255)
                     .IsRequired();
-
                 entity.Property(e => e.Cost)
                     .HasColumnName("Cost")
                     .IsRequired();
-
                 entity.HasOne(x => x.ProductCategory)
                     .WithMany(c => c.Products)
-                    .HasForeignKey(x => x.Id)
+                    .HasForeignKey(x => x.CategoryId) // Изменение внешнего ключа на CategoryId
                     .HasConstraintName("CategoryToProduct");
             });
 
-            modelBuilder.Entity<ProductCategory>(entity => 
+            modelBuilder.Entity<ProductCategory>(entity =>
             {
                 entity.ToTable("ProductCategory");
-
+                entity.Property(x => x.Id).ValueGeneratedOnAdd(); // Указание, что Id будет генерироваться автоматически
                 entity.HasKey(x => x.Id);
                 entity.HasIndex(x => x.Name).IsUnique();
-
                 entity.Property(e => e.Name)
                     .HasColumnName("CategoryName")
                     .HasMaxLength(255)
@@ -62,15 +58,12 @@ namespace Example.Models
             modelBuilder.Entity<ProductsStorage>(entity =>
             {
                 entity.ToTable("Sorage");
-
+                entity.Property(x => x.Id).ValueGeneratedOnAdd(); // Указание, что Id будет генерироваться автоматически
                 entity.HasKey(x => x.Id).HasName("ProductsStorageId");
-
                 entity.Property(entity => entity.Name)
                     .HasColumnName("ProductsStorageName");
-
                 entity.Property(e => e.Amount)
                     .HasColumnName("ProductAmount");
-
                 entity.HasMany(x => x.Products)
                     .WithMany(m => m.ProductsStorage)
                     .UsingEntity(j => j.ToTable("ProductsStorage"));
